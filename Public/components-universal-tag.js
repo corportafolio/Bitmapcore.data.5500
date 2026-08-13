ef467d32f49184fd5abb0ccdf5aba0d8049a7535
Candela <candela@bitmapcore.net>
2026-08-12 21:27:21 -0500

    fix: tag text bright orange #FE3E00 on metallic background

--- Public/components-universal-tag.js
+++ Public/components-universal-tag.js
@@ -10,7 +10,7 @@ function UniversalTag(props) {
     style: {
       display: 'inline-block',
       backgroundColor: '#8B2500',
-      color: '#000000',
+      color: '#FE3E00',
       fontFamily: "'Alfa Slab One', serif",
       fontWeight: 'bold',
       fontSize: fontSize + 'px',
