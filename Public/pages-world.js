32ac398d5975e1bc903b257418d4ef7460aaa787
Candela <candela@bitmapcore.net>
2026-08-22 16:47:41 -0500

    fix: initial camera at block 0 (equator, longitude 0°)

--- Public/pages-world.js
+++ Public/pages-world.js
@@ -201,7 +201,7 @@ var PagesWorld = (function() {
       null
     );
 
-    WorldControls.setInitial(0, 0, 280);
+    WorldControls.setInitial(-Math.PI / 2, 0, 280);
     WorldControls.setOnChange(onChange);
 
     var initialState = WorldControls.getState();
